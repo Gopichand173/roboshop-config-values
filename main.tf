8f95867767d558cba1339d8f6895dafb60dcc7e0
Gopi Chand <gopi110531@gmail.com>
2026-09-16 10:35:22 -0500

    Roboshop code

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
-resource "vault_mount" "component"
+resource "vault_mount" "component" {
 #vault_mount means engine
-{
-  path        = "cart-dev"
-  type        = "kv-v2"
+path        = "cart-dev"
+type        = "kv-v2"
   options = {
     version = "2"
     type    = "kv-v2"
