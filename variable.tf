3123d9e42d030ca5eb62d294c53610fea688c65c
Gopi Chand <gopi110531@gmail.com>
2026-09-22 12:35:52 -0500

    Roboshop code

--- variable.tf
+++ variable.tf
@@ -28,5 +28,12 @@ variable "secret-mounts" {
           PAYMENT_URL = "http://shipping-dev.gdevops72.com:8080/"
          }
 	 }
+     cart = {
+		secret_mount = "roboshop-dev"
+        kv = {
+        MONGO_URL     = "mongodb://mongodb-dev.gdevops72.com:27017/catalogue",
+        MONGO = "true"
+       }
+	 }
    }
  }
